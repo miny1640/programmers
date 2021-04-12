7ee2e4bd5cb472882b28c4ea90dd9a606adc310f
LAPTOP-28VPB5JH\pmy <miny1640@naver.com>
2021-04-12 16:39:43 +0900

    clean up code

--- programmers/src/hash/UnfinishedRunnerExcute.java
+++ programmers/src/hash/UnfinishedRunnerExcute.java
@@ -8,8 +8,15 @@ public class UnfinishedRunnerExcute {
 		// TODO Auto-generated method stub
 		UnfinishedRunner ur = new UnfinishedRunner();
 		
-		String[][] participant = {{"leo", "kiki", "eden"},{"marina", "josipa", "nikola", "vinko", "filipa"},{"mislav", "stanko", "mislav", "ana"}};
-		String[][] completion = {{"eden", "kiki"},{"josipa", "filipa", "marina", "nikola"},{"stanko", "ana", "mislav"}};
+		String[][] participant = {	{"leo", "kiki", "eden"},
+									{"marina", "josipa", "nikola", "vinko", "filipa"},
+									{"mislav", "stanko", "mislav", "ana"}
+								};
+		
+		String[][] completion = {	{"eden", "kiki"},
+									{"josipa", "filipa", "marina", "nikola"},
+									{"stanko", "ana", "mislav"}
+								};
 		
 		for(int i = 0 ; i < participant.length ; i++) {
 			System.out.println(ur.solution(participant[i], completion[i]));
