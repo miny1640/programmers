c450185bc36f7670765a2e83a3aa5f620405760a
LAPTOP-28VPB5JH\pmy <miny1640@naver.com>
2021-04-13 16:23:51 +0900

    Commit correct answer

--- programmers/src/hash/BestAlbum.java
+++ programmers/src/hash/BestAlbum.java
@@ -20,6 +20,14 @@ public class BestAlbum {
         	}
         }
         
+        System.out.println("genreList.size() = " + genreList.size());
+        System.out.println();
+        System.out.println("genrePlay<Key : value>");
+        for(int i = 0 ; i < genreList.size() ; i++) {
+        	System.out.println(genreList.get(i) +  " : " + genrePlay.get(genreList.get(i)));
+        }
+        System.out.println();
+        
         String[] genreRank = new String[genreList.size()];
         
         for(int i = 0 ; i < genreList.size() ; i++) {
@@ -31,6 +39,12 @@ public class BestAlbum {
         	}
         	genreRank[rank] = genreList.get(i);
         }
+
+        System.out.println("genreRank[i] = value");
+        for(int i = 0 ; i < genreRank.length ; i++) {
+            System.out.println("genreRank[" + i + "] = " + genreRank[i]);
+        }
+        System.out.println();
         
         for(int i = 0 ; i < genreRank.length ; i++) {
         	ArrayList<Integer> songList = new ArrayList<Integer>();
@@ -39,6 +53,12 @@ public class BestAlbum {
         			songList.add(j);
         		}
         	}
+        	
+            System.out.println(genreRank[i] + " songList(j) : value");
+            for(int j = 0 ; j < songList.size() ; j++) {
+            	System.out.println("songList(" + j + ") : " + songList.get(j));
+            }
+            System.out.println();
 
     		int max = 0;
     		int topSong = 0;
@@ -50,7 +70,7 @@ public class BestAlbum {
         		}
         	}
         	
-        	songRank.add(topSong);
+        	songRank.add(songList.get(topSong));
         	songList.remove(topSong);
         	
         	max = 0;
@@ -61,8 +81,8 @@ public class BestAlbum {
         			topSong = j;
         		}
         	}
-        	
-        	songRank.add(topSong);
+
+        	songRank.add(songList.get(topSong));
         }
         
         int[] answer = new int[songRank.size()];
